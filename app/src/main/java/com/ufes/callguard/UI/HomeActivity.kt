2eb9e19d416fa9cf08207454ca90967655b8f27c
Matheus Lopes <matheulflim@gmail.com>
2024-06-13 18:44:56 -0300

    Configurado a homeActivity

--- app/src/main/java/com/ufes/callguard/UI/HomeActivity.kt
+++ app/src/main/java/com/ufes/callguard/UI/HomeActivity.kt
@@ -1,21 +1,38 @@
 package com.ufes.callguard.UI
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ufes.callguard.R
+import com.ufes.callguard.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ButtonConfig.setOnClickListener { view ->
+            startActivity(Intent(this, ConfigurationActivity::class.java))
         }
+
+        binding.ButtonBloquear.setOnClickListener { view ->
+            startActivity(Intent(this, BlockActivity::class.java))
+        }
+
+        binding.ButtonHistorico.setOnClickListener { view ->
+            startActivity(Intent(this, HistoricHomeActivity::class.java))
+        }
+
+        binding.ButtonComunidade.setOnClickListener { view ->
+            startActivity(Intent(this, CommunityActivity::class.java))
+        }
+
     }
 }
