aade383e650aa8def1d92e0e173f66b88d66a4b1
Matheus <matheusdeoliveiraes@gmail.com>
2024-06-26 21:09:31 -0300

    Fiz o front de person com cor e sem cor, pra identificar quem é um contato e quem é desconhecido

--- app/src/main/java/com/ufes/callguard/Util/ContactAdapter.kt
+++ app/src/main/java/com/ufes/callguard/Util/ContactAdapter.kt
@@ -1,14 +1,19 @@
 package com.ufes.callguard.Util
 
+import android.content.Context
+import android.database.Cursor
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ufes.callguard.Class.Contact
 import com.ufes.callguard.R
 
 class ContactAdapter(
+    private val context: Context,
     private val contactList: List<Contact>,
     private val onItemClick: (Contact) -> Unit
 ) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
@@ -16,6 +21,7 @@ class ContactAdapter(
     class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val contactName: TextView = itemView.findViewById(R.id.contactName)
         val contactNumber: TextView = itemView.findViewById(R.id.contactNumber)
+        val contactImage: ImageView = itemView.findViewById(R.id.contactImage)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
@@ -27,10 +33,34 @@ class ContactAdapter(
         val contact = contactList[position]
         holder.contactName.text = contact.getName()
         holder.contactNumber.text = contact.getNumber()
+
+        if (isNumberInContacts(contact.getNumber())) {
+            holder.contactImage.setImageResource(R.drawable.baseline_person_green_24)
+        } else {
+            holder.contactImage.setImageResource(R.drawable.baseline_person_24)
+        }
+
         holder.itemView.setOnClickListener {
             onItemClick(contact)
         }
     }
 
     override fun getItemCount() = contactList.size
+
+    private fun isNumberInContacts(number: String): Boolean {
+        val uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER)
+        val cursor: Cursor? = context.contentResolver.query(uri, projection, null, null, null)
+        cursor?.use {
+            val numberIndex = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            while (it.moveToNext()) {
+                val contactNumber = it.getString(numberIndex)?.replace(Regex("[^0-9]"), "")
+                if (contactNumber == number) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
 }
