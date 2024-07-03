ed21c74178a4bea6c3d3d910fa6fd01d7d3a2745
Matheus <matheusdeoliveiraes@gmail.com>
2024-07-03 20:22:54 -0300

    Preciso testar se está funcionando a aparição dos números da lista de bloqueados dos amigos

--- app/src/main/java/com/ufes/callguard/Class/BlockedContactModel.kt
+++ app/src/main/java/com/ufes/callguard/Class/BlockedContactModel.kt
@@ -1,3 +1,10 @@
 package com.ufes.callguard.Class
 
-data class BlockedContactModel(val name: String, val number: String)
+data class BlockedContactModel(
+    val name: String,
+    val number: String
+) {
+    fun toMap(): Map<String, String> {
+        return mapOf("name" to name, "number" to number)
+    }
+}
